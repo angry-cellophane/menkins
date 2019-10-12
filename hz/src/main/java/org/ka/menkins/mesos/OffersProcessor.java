4de15e8d165fc1d0ec3a12ab84b05ed27c80850e
angry-cellophane <email.com>
2019-10-12 23:35:08 +0100

    Add scheduler

--- hz/src/main/java/org/ka/menkins/mesos/OffersProcessor.java
+++ hz/src/main/java/org/ka/menkins/mesos/OffersProcessor.java
@@ -2,22 +2,19 @@ package org.ka.menkins.mesos;
 
 import lombok.AllArgsConstructor;
 import org.apache.mesos.Protos;
-import org.apache.mesos.SchedulerDriver;
 import org.ka.menkins.queue.BuilderNodeRequest;
 
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 @AllArgsConstructor
 public class OffersProcessor implements Consumer<List<Protos.Offer>> {
 
-    Supplier<SchedulerDriver> driverHolder;
+    Schedulers.State state;
     BlockingQueue<BuilderNodeRequest> requests;
 
     @Override
     public void accept(List<Protos.Offer> offers) {
-
     }
 }
--- hz/src/main/java/org/ka/menkins/mesos/Schedulers.java
+++ hz/src/main/java/org/ka/menkins/mesos/Schedulers.java
@@ -1,26 +1,51 @@
 package org.ka.menkins.mesos;
 
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.mesos.MesosSchedulerDriver;
 import org.apache.mesos.Protos;
 import org.apache.mesos.SchedulerDriver;
 import org.ka.menkins.app.AppConfig;
 import org.ka.menkins.queue.BuilderNodeRequest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
+@Slf4j
 public class Schedulers {
+
+    @Value
+    static class State {
+        BlockingQueue<List<BuilderNodeRequest>> localQueue;
+        AtomicBoolean suppress;
+        AtomicReference<SchedulerDriver> driver;
+    }
 
     private Schedulers() {}
 
-    public static Runnable newInitializer(AppConfig config, BlockingQueue<BuilderNodeRequest> queue) {
+    public static Runnable newInitializer(AppConfig config, BlockingQueue<BuilderNodeRequest> globalQueue) {
         return () -> {
-            var driverHolder = new AtomicReference<SchedulerDriver>();
-            var offersProcessor = new OffersProcessor(() -> driverHolder.get(), queue);
+            log.info("Initializing driver");
 
+            var state = new State(new ArrayBlockingQueue<>(5),
+                    new AtomicBoolean(false),
+                    new AtomicReference<>(null));
+
+            var offersProcessor = new OffersProcessor(state, globalQueue);
             var scheduler = new MenkinsScheduler(offersProcessor);
 
-//            var driver = new MesosSchedulerDriver(scheduler, )
+            var driver = new MesosSchedulerDriver(scheduler, newFrameworkInfo(config), config.getMesos().getMesosMasterUrl());
+            state.driver.set(driver);
+            startDriver(driver);
+
+            startBufferThread(state, globalQueue);
+
+            log.info("driver initialized");
         };
     }
 
@@ -32,5 +57,51 @@ public class Schedulers {
                 .setCheckpoint(config.getMesos().isCheckpoint())
                 .setWebuiUrl(config.getUrl())
                 .build();
+    }
+
+    private static void startDriver(SchedulerDriver driver) {
+        var thread = new Thread(() -> {
+            var status = driver.run();
+            if (status != Protos.Status.DRIVER_STOPPED) {
+                log.error("driver was aborted, status " + status.getNumber());
+            } else {
+                log.info("driver stopped");
+            }
+        });
+        thread.setName("driver-status-thread");
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    private static void startBufferThread(State state, BlockingQueue<BuilderNodeRequest> globalQueue) {
+        var BUFFER_SIZE = 5;
+        var TIME_LIMIT = TimeUnit.SECONDS.toMillis(5);
+        var thread = new Thread(() -> {
+            var local = new ArrayList<BuilderNodeRequest>(BUFFER_SIZE);
+            var newGroupCreated = System.currentTimeMillis();
+            for (;;) {
+                try {
+                    var value = globalQueue.poll(1, TimeUnit.SECONDS);
+                    if (value != null) local.add(value);
+
+                    var size = local.size();
+                    if (size > 0 && (size >= BUFFER_SIZE || (System.currentTimeMillis() - newGroupCreated > TIME_LIMIT))) {
+                        state.localQueue.add(local);
+                        local = new ArrayList<>(BUFFER_SIZE);
+                        newGroupCreated = System.currentTimeMillis();
+
+                        if (state.suppress.get()) {
+                            state.suppress.set(false);
+                            state.driver.get().reviveOffers();
+                        }
+                    }
+                } catch (Exception e) {
+                    log.error("error in global queue to local thread", e);
+                }
+            }
+        });
+        thread.setName("global-to-local-queue");
+        thread.setDaemon(true);
+        thread.start();
     }
 }
