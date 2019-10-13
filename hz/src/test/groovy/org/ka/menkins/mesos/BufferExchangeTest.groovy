4ca33aab627b0ed9fe24bfd09fb023c13c7acfc9
angry-cellophane <email.com>
2019-10-13 23:49:03 +0100

    Add tests for BufferHandler

--- hz/src/test/groovy/org/ka/menkins/mesos/BufferExchangeTest.groovy
+++ hz/src/test/groovy/org/ka/menkins/mesos/BufferExchangeTest.groovy
@@ -4,7 +4,10 @@ import org.apache.mesos.SchedulerDriver
 import org.ka.menkins.queue.NodeRequestWithResources
 import spock.lang.Specification
 
-import java.util.concurrent.*
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicReference
 
