48c887713d51987d72af2c46bcfd418c250bc976
angry-cellophane <email.com>
2019-10-18 00:53:45 +0100

    Parse storage properties from env vars and system properties

---
+++ server/src/test/groovy/org/ka/menkins/app/init/StorageConfigurationTest.groovy
@@ -0,0 +1,48 @@
+package org.ka.menkins.app.init
+
+import spock.lang.Specification
+
+class StorageConfigurationTest extends Specification {
+
+    void 'load all properties from env vars'() {
+        given:
+        def props = new Properties()
+        def env = [
+                (StorageConfiguration.TYPE.envName): 'local',
+        ]
+        def holder = new PropertiesHolder(env, props)
+
+        when:
+        def builder = StorageConfiguration.load(holder).apply(AppConfig.builder())
+
+        then:
+        builder.storageType == AppConfig.StorageType.LOCAL
+    }
+
+    void 'load all properties from system properties'() {
+        given:
+        def props = new Properties()
+        props.put(StorageConfiguration.TYPE.propertyName, 'hazelcast')
+        def env = [:]
+        def holder = new PropertiesHolder(env, props)
+
+        when:
+        def builder = StorageConfiguration.load(holder).apply(AppConfig.builder())
+
+        then:
+        builder.storageType == AppConfig.StorageType.HAZELCAST
+    }
+
+    void 'check default values are set'() {
+        given:
+        def props = new Properties()
+        def env = [:]
+        def holder = new PropertiesHolder(env, props)
+
+        when:
+        def builder = StorageConfiguration.load(holder).apply(AppConfig.builder())
+
+        then:
+        builder.storageType == AppConfig.StorageType.LOCAL
+    }
+}
