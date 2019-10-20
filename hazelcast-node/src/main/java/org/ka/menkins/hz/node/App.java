f8ae2a4bbd6466a43c0cb2dbfc4ac2d26426ac69
angry-cellophane <email.com>
2019-10-20 20:37:12 +0100

    Move hazelcast data nodes to a separate project, use hazelcast client in server

--- hazelcast-node/src/main/java/org/ka/menkins/hz/node/App.java
+++ hazelcast-node/src/main/java/org/ka/menkins/hz/node/App.java
@@ -1,4 +1,29 @@
 package org.ka.menkins.hz.node;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.core.Hazelcast;
+
 public class App {
+    public static void main(String[] args) {
+        var config1 = new Config();
+        config1.getNetworkConfig().setPort(5701);
+        var hz1 = Hazelcast.newHazelcastInstance(config1);
+
+        var config2 = new Config();
+        config2.getNetworkConfig().setPort(5702);
+
+        var joinConfig = new JoinConfig();
+        joinConfig.getTcpIpConfig().setEnabled(true);
+        joinConfig.getTcpIpConfig().addMember("localhost:5701");
+        joinConfig.getMulticastConfig().setEnabled(false);
+        config2.getNetworkConfig().setJoin(joinConfig);
+
+        var hz2 = Hazelcast.newHazelcastInstance(config1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            hz1.getLifecycleService().shutdown();
+            hz2.getLifecycleService().shutdown();
+        }));
+    }
 }
